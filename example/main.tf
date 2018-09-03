1305aa12ff9c9937dcb25ffff1c7151750c0bfd9
Michael Parker <michael@parker.gg>
2018-09-03 14:07:14 -0500

    Updates example config

--- example/main.tf
+++ example/main.tf
@@ -5,9 +5,16 @@ provider "keycloak" {
 }
 
 resource "keycloak_realm" "test" {
-  realm             = "test"
-  enabled           = false
-  display_name      = "foo"
+  realm                          = "test"
+  enabled                        = false
+  display_name                   = "foo"
 
-  registration_allowed = true
+  registration_allowed           = true
+  registration_email_as_username = false
+  edit_username_allowed          = false
+  reset_password_allowed         = true
+  remember_me                    = false
+  verify_email                   = true
+  login_with_email_allowed       = true
+  duplicate_emails_allowed       = false
 }
