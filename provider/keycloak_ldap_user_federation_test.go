3755fbe6a36709bb3fc6388bb1cd01e82074a058
Michael Parker <michael@parker.gg>
2018-11-17 15:42:59 -0600

    fix: keycloak_ldap_user_federation - track correct bind credential in state (#63)

--- provider/keycloak_ldap_user_federation_test.go
+++ provider/keycloak_ldap_user_federation_test.go
@@ -24,8 +24,37 @@ func TestAccKeycloakLdapUserFederation_basic(t *testing.T) {
 				Config: testKeycloakLdapUserFederation_basic(realmName, ldapName),
 				Check:  testAccCheckKeycloakLdapUserFederationExists("keycloak_ldap_user_federation.openldap"),
 			},
+		},
+	})
+}
+
+func TestAccKeycloakLdapUserFederation_import(t *testing.T) {
+	realmName := "terraform-" + acctest.RandString(10)
+	ldapName := "terraform-" + acctest.RandString(10)
+
+	bindCredentialForImport := "admin"
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckKeycloakLdapUserFederationDestroy(),
+		Steps: []resource.TestStep{
 			{
-				ResourceName:        "keycloak_ldap_user_federation.openldap",
+				Config: testKeycloakLdapUserFederation_basic(realmName, ldapName),
+				Check:  testAccCheckKeycloakLdapUserFederationExists("keycloak_ldap_user_federation.openldap"),
+			},
+			{
+				ResourceName:      "keycloak_ldap_user_federation.openldap",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: getLdapUserFederationImportId("keycloak_ldap_user_federation.openldap", bindCredentialForImport),
+			},
+			{
+				Config: testKeycloakLdapUserFederation_noAuth(realmName, ldapName),
+				Check:  testAccCheckKeycloakLdapUserFederationExists("keycloak_ldap_user_federation.openldap_no_auth"),
+			},
+			{
+				ResourceName:        "keycloak_ldap_user_federation.openldap_no_auth",
 				ImportState:         true,
 				ImportStateVerify:   true,
 				ImportStateIdPrefix: realmName + "/",
@@ -359,6 +388,29 @@ func TestAccKeycloakLdapUserFederation_syncPeriodValidation(t *testing.T) {
 	})
 }
 
+func TestAccKeycloakLdapUserFederation_bindCredential(t *testing.T) {
+	realmName := "terraform-" + acctest.RandString(10)
+	ldapName := "terraform-" + acctest.RandString(10)
+	firstBindCredential := acctest.RandString(10)
+	secondBindCredential := acctest.RandString(10)
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckKeycloakLdapUserFederationDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakLdapUserFederation_bindCredential(realmName, ldapName, firstBindCredential),
+				Check:  resource.TestCheckResourceAttr("keycloak_ldap_user_federation.openldap", "bind_credential", firstBindCredential),
+			},
+			{
+				Config: testKeycloakLdapUserFederation_bindCredential(realmName, ldapName, secondBindCredential),
+				Check:  resource.TestCheckResourceAttr("keycloak_ldap_user_federation.openldap", "bind_credential", secondBindCredential),
+			},
+		},
+	})
+}
+
 func testAccCheckKeycloakLdapUserFederationExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		_, err := getLdapUserFederationFromState(s, resourceName)
@@ -423,6 +475,20 @@ func getLdapUserFederationFromState(s *terraform.State, resourceName string) (*k
 	}
 
 	return ldap, nil
+}
+
+func getLdapUserFederationImportId(resourceName, bindCredential string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("resource not found: %s", resourceName)
+		}
+
+		id := rs.Primary.ID
+		realmId := rs.Primary.Attributes["realm_id"]
+
+		return fmt.Sprintf("%s/%s/%s", realmId, id, bindCredential), nil
+	}
 }
 
 func testKeycloakLdapUserFederation_basic(realm, ldap string) string {
@@ -628,6 +694,58 @@ resource "keycloak_ldap_user_federation" "openldap" {
 
 	connection_timeout      = "10s"
 	read_timeout            = "5s"
+}
+	`, realm, ldap)
+}
+
+func testKeycloakLdapUserFederation_bindCredential(realm, ldap, bindCredential string) string {
+	return fmt.Sprintf(`
+resource "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_ldap_user_federation" "openldap" {
+	name                    = "%s"
+	realm_id                = "${keycloak_realm.realm.id}"
+
+	enabled                 = true
+
+	username_ldap_attribute = "cn"
+	rdn_ldap_attribute      = "cn"
+	uuid_ldap_attribute     = "entryDN"
+	user_object_classes     = [
+		"simpleSecurityObject",
+		"organizationalRole"
+	]
+	connection_url          = "ldap://openldap"
+	users_dn                = "dc=example,dc=org"
+	bind_dn                 = "cn=admin,dc=example,dc=org"
+	bind_credential         = "%s"
+}
+	`, realm, ldap, bindCredential)
+}
+
+func testKeycloakLdapUserFederation_noAuth(realm, ldap string) string {
+	return fmt.Sprintf(`
+resource "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_ldap_user_federation" "openldap_no_auth" {
+	name                    = "%s"
+	realm_id                = "${keycloak_realm.realm.id}"
+
+	enabled                 = true
+
+	username_ldap_attribute = "cn"
+	rdn_ldap_attribute      = "cn"
+	uuid_ldap_attribute     = "entryDN"
+	user_object_classes     = [
+		"simpleSecurityObject",
+		"organizationalRole"
+	]
+	connection_url          = "ldap://openldap"
+	users_dn                = "dc=example,dc=org"
 }
 	`, realm, ldap)
 }
