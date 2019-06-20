0e36511a9266bc3a64274b235b9150affbc4bf0b
Florent Jaby <florent.jaby@gmail.com>
2019-06-20 15:36:52 +0200

    chore: build a specific linux target with CGO_ENABLED=0 (#129)

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -9,12 +9,20 @@ mkdir -p ../artifacts
 for config in $(cat release-targets.json | jq -rc '.[]'); do
 	os=$(echo ${config} | jq -r '.os')
 	platform=$(echo ${config} | jq -r '.platform')
+	static=$(echo ${config} | jq -r '.static // false')
+  linkage=''
+  if [[ ${static} = 'true' ]]; then
+    export CGO_ENABLED=0
+    linkage='_static'
+  else
+    unset CGO_ENABLED
+  fi
 
-	echo "Building for ${os}_${platform}..."
+	echo "Building for ${os}_${platform}${linkage}..."
 
 	GOOS=${os} GOARCH=${platform} go build -o terraform-provider-keycloak_v${CIRCLE_TAG} ..
-	zip terraform-provider-keycloak_v${CIRCLE_TAG}_${os}_${platform}.zip terraform-provider-keycloak_v${CIRCLE_TAG} ../LICENSE
-	mv terraform-provider-keycloak_v${CIRCLE_TAG}_${os}_${platform}.zip ../artifacts
+	zip terraform-provider-keycloak_v${CIRCLE_TAG}_${os}_${platform}${linkage}.zip terraform-provider-keycloak_v${CIRCLE_TAG} ../LICENSE
+	mv terraform-provider-keycloak_v${CIRCLE_TAG}_${os}_${platform}${linkage}.zip ../artifacts
 	rm terraform-provider-keycloak_v${CIRCLE_TAG}
 done;
 
