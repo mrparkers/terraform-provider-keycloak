233da2964b8236a34f66171494812d53dce09d22
Tom Rutsaert <tom.rutsaert@axians.com>
2019-06-25 10:19:42 +0200

    enable security defenses in example

--- example/main.tf
+++ example/main.tf
@@ -26,21 +26,7 @@ resource "keycloak_realm" "test" {
 		}
 	}
 
-//  security_defenses {
-//	  headers {
-//		  x_frame_options = "DENY"
-//		  content_security_policy = "frame-src 'self'; frame-ancestors 'self'; object-src 'none';"
-//		  content_security_policy_report_only = ""
-//		  x_content_type_options = "nosniff"
-//		  x_robots_tag = "none"
-//		  x_xss_protection = "1; mode=block"
-//		  strict_transport_security = "max-age=31536000; includeSubDomains"
-//	  }
-//
-//	  // todo add bruto force detection example
-//  }
-
-  account_theme = "base"
+  	account_theme = "base"
 
 	access_code_lifespan = "30m"
 
@@ -51,6 +37,20 @@ resource "keycloak_realm" "test" {
 			"es"
 		]
 		default_locale    = "en"
+	}
+
+	security_defenses {
+		headers {
+			x_frame_options = "DENY"
+			content_security_policy = "frame-src 'self'; frame-ancestors 'self'; object-src 'none';"
+			content_security_policy_report_only = ""
+			x_content_type_options = "nosniff"
+			x_robots_tag = "none"
+			x_xss_protection = "1; mode=block"
+			strict_transport_security = "max-age=31536000; includeSubDomains"
+		}
+
+		// todo add bruto force detection example
 	}
 }
 
