343dafb5ed50da75167af1e32edf777f3bd29229
Michael Parker <michael@parker.gg>
2019-08-18 13:07:09 -0500

    adds role update tests

--- provider/resource_keycloak_role_test.go
+++ provider/resource_keycloak_role_test.go
@@ -56,6 +56,61 @@ func TestAccKeycloakRole_basicClient(t *testing.T) {
 	})
 }
 
+func TestAccKeycloakRole_basicRealmUpdate(t *testing.T) {
+	realmName := "terraform-" + acctest.RandString(10)
+	roleName := "terraform-role-" + acctest.RandString(10)
+	descriptionOne := acctest.RandString(50)
+	descriptionTwo := acctest.RandString(50)
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckKeycloakRoleDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRole_basicRealmWithDescription(realmName, roleName, descriptionOne),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+			{
+				Config: testKeycloakRole_basicRealmWithDescription(realmName, roleName, descriptionTwo),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+			{
+				Config: testKeycloakRole_basicRealm(realmName, roleName),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+		},
+	})
+}
+
+func TestAccKeycloakRole_basicClientUpdate(t *testing.T) {
+	realmName := "terraform-" + acctest.RandString(10)
+	clientId := "terraform-client-" + acctest.RandString(10)
+	roleName := "terraform-role-" + acctest.RandString(10)
+	descriptionOne := acctest.RandString(50)
+	descriptionTwo := acctest.RandString(50)
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckKeycloakRoleDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRole_basicClientWithDescription(realmName, clientId, roleName, descriptionOne),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+			{
+				Config: testKeycloakRole_basicClientWithDescription(realmName, clientId, roleName, descriptionTwo),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+			{
+				Config: testKeycloakRole_basicClient(realmName, clientId, roleName),
+				Check:  testAccCheckKeycloakRoleExists("keycloak_role.role"),
+			},
+		},
+	})
+}
+
 func testAccCheckKeycloakRoleExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		_, err := getRoleFromState(s, resourceName)
@@ -121,6 +176,20 @@ resource "keycloak_role" "role" {
 	`, realm, role)
 }
 
+func testKeycloakRole_basicRealmWithDescription(realm, role, description string) string {
+	return fmt.Sprintf(`
+resource "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_role" "role" {
+	name        = "%s"
+	description = "%s"
+	realm_id    = "${keycloak_realm.realm.id}"
+}
+	`, realm, role, description)
+}
+
 func testKeycloakRole_basicClient(realm, clientId, role string) string {
 	return fmt.Sprintf(`
 resource "keycloak_realm" "realm" {
@@ -139,4 +208,25 @@ resource "keycloak_role" "role" {
 	client_id = "${keycloak_openid_client.client.id}"
 }
 	`, realm, clientId, role)
+}
+
+func testKeycloakRole_basicClientWithDescription(realm, clientId, role, description string) string {
+	return fmt.Sprintf(`
+resource "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_openid_client" "client" {
+	client_id   = "%s"
+	realm_id    = "${keycloak_realm.realm.id}"
+	access_type = "CONFIDENTIAL"
+}
+
+resource "keycloak_role" "role" {
+	name        = "%s"
+	realm_id    = "${keycloak_realm.realm.id}"
+	client_id   = "${keycloak_openid_client.client.id}"
+	description = "%s"
+}
+	`, realm, clientId, role, description)
 }
