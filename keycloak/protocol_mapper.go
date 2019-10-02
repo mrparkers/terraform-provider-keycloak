a5632b43d0d9293a2d1acb88b168fa4692d3bb5b
Marco Reni <marco.reni@drivek.com>
2019-10-02 13:36:34 +0200

    New resource openid_user_realm_role_protocol_mapper

--- keycloak/protocol_mapper.go
+++ keycloak/protocol_mapper.go
@@ -12,21 +12,22 @@ type protocolMapper struct {
 }
 
 var (
-	addToAccessTokenField       = "access.token.claim"
-	addToIdTokenField           = "id.token.claim"
-	addToUserInfoField          = "userinfo.token.claim"
-	attributeNameField          = "attribute.name"
-	attributeNameFormatField    = "attribute.nameformat"
-	claimNameField              = "claim.name"
-	claimValueField             = "claim.value"
-	claimValueTypeField         = "jsonType.label"
-	friendlyNameField           = "friendly.name"
-	fullPathField               = "full.path"
-	includedClientAudienceField = "included.client.audience"
-	includedCustomAudienceField = "included.custom.audience"
-	multivaluedField            = "multivalued"
-	userAttributeField          = "user.attribute"
-	userPropertyField           = "user.attribute"
+	addToAccessTokenField               = "access.token.claim"
+	addToIdTokenField                   = "id.token.claim"
+	addToUserInfoField                  = "userinfo.token.claim"
+	attributeNameField                  = "attribute.name"
+	attributeNameFormatField            = "attribute.nameformat"
+	claimNameField                      = "claim.name"
+	claimValueField                     = "claim.value"
+	claimValueTypeField                 = "jsonType.label"
+	friendlyNameField                   = "friendly.name"
+	fullPathField                       = "full.path"
+	includedClientAudienceField         = "included.client.audience"
+	includedCustomAudienceField         = "included.custom.audience"
+	multivaluedField                    = "multivalued"
+	userAttributeField                  = "user.attribute"
+	userPropertyField                   = "user.attribute"
+	userRealmRoleMappingRolePrefixField = "usermodel.realmRoleMapping.rolePrefix"
 )
 
 func protocolMapperPath(realmId, clientId, clientScopeId string) string {
