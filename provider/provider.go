421d67bff254be8dadf6b72fe45d7eeb4b80cb9b
tomrutsaert <tomrutsaert@users.noreply.github.com>
2020-03-04 16:56:20 +0100

    new resource: keycloak_oidc_google_identity_provider (#240)

--- provider/provider.go
+++ provider/provider.go
@@ -54,6 +54,7 @@ func KeycloakProvider() *schema.Provider {
 			"keycloak_attribute_to_role_identity_provider_mapper":      resourceKeycloakAttributeToRoleIdentityProviderMapper(),
 			"keycloak_user_template_importer_identity_provider_mapper": resourceKeycloakUserTemplateImporterIdentityProviderMapper(),
 			"keycloak_saml_identity_provider":                          resourceKeycloakSamlIdentityProvider(),
+			"keycloak_oidc_google_identity_provider":                   resourceKeycloakOidcGoogleIdentityProvider(),
 			"keycloak_oidc_identity_provider":                          resourceKeycloakOidcIdentityProvider(),
 			"keycloak_openid_client_authorization_resource":            resourceKeycloakOpenidClientAuthorizationResource(),
 			"keycloak_openid_client_authorization_scope":               resourceKeycloakOpenidClientAuthorizationScope(),
