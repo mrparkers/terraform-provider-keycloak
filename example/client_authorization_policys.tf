6c81b6132e0e3493e61b4e8ed2b5f8ed4d4772ca
Michael Parker <michael@parker.gg>
2020-03-10 10:05:40 -0500

    fix client authorization policy example

--- example/client_authorization_policys.tf
+++ example/client_authorization_policys.tf
@@ -1,15 +1,15 @@
-resource keycloak_realm_authorization test {
-  realm                = "%s"
+resource keycloak_realm test_authorization {
+  realm                = "test_authorization"
   enabled              = true
   display_name         = "foo"
   account_theme        = "base"
   access_code_lifespan = "30m"
 }
 
-resource keycloak_openid_client_authorization_authorization test {
+resource keycloak_openid_client test {
   client_id                = "test-openid-client"
   name                     = "test-openid-client"
-  realm_id                 = "${keycloak_realm_authorization.test.id}"
+  realm_id                 = "${keycloak_realm.test_authorization.id}"
   description              = "a test openid client"
   standard_flow_enabled    = true
   service_accounts_enabled = true
@@ -23,62 +23,62 @@ resource keycloak_openid_client_authorization_authorization test {
   }
 }
 
-# 
+#
 # create aggregate_policy
-# 
+#
 
-resource keycloak_role_aggregate test {
-  realm_id = "${keycloak_realm_authorization.test.id}"
+resource keycloak_role test_authorization {
+  realm_id = "${keycloak_realm.test_authorization.id}"
   name     = "aggregate_policy_role"
 }
 
-resource keycloak_openid_client_authorization_role_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
-  name               = "keycloak_openid_client_authorization_role_policy"
+resource keycloak_openid_client_role_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
+  name               = "keycloak_openid_client_role_policy"
   decision_strategy  = "UNANIMOUS"
   logic              = "POSITIVE"
   type               = "role"
   role {
-    id       = "${keycloak_role_aggregate.test.id}"
+    id       = "${keycloak_role.test_authorization.id}"
     required = false
   }
 }
 
-resource keycloak_openid_client_authorization_aggregate_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
-  name               = "keycloak_openid_client_authorization_aggregate_policy"
+resource keycloak_openid_client_aggregate_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
+  name               = "keycloak_openid_client_aggregate_policy"
   decision_strategy  = "UNANIMOUS"
   logic              = "POSITIVE"
-  policies           = ["${keycloak_openid_client_authorization_role_policy.test.id}"]
+  policies           = ["${keycloak_openid_client_role_policy.test.id}"]
 }
 
-# 
-# create client policy 
-# 
+#
+# create client policy
+#
 
-resource keycloak_openid_client_authorization_client_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
-  name               = "keycloak_openid_client_authorization_client_policy"
+resource keycloak_openid_client_client_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
+  name               = "keycloak_openid_client_client_policy"
   decision_strategy  = "AFFIRMATIVE"
   logic              = "POSITIVE"
-  clients            = ["${keycloak_openid_client_authorization.test.resource_server_id}"]
+  clients            = ["${keycloak_openid_client.test.resource_server_id}"]
 }
 
-# 
+#
 # create group policy
-# 
+#
 
 resource keycloak_group test {
-  realm_id = "${keycloak_realm_authorization.test.id}"
+  realm_id = "${keycloak_realm.test_authorization.id}"
   name     = "foo"
 }
 
-resource keycloak_openid_client_authorization_group_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
+resource keycloak_openid_client_group_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
   name               = "client_group_policy_test"
   groups {
     id              = "${keycloak_group.test.id}"
@@ -90,13 +90,13 @@ resource keycloak_openid_client_authorization_group_policy test {
 }
 
 
-# 
+#
 # create JS policy
-# 
+#
 
-resource keycloak_openid_client_authorization_js_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
+resource keycloak_openid_client_js_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
   name               = "client_js_policy_test"
   logic              = "POSITIVE"
   decision_strategy  = "UNANIMOUS"
@@ -105,35 +105,35 @@ resource keycloak_openid_client_authorization_js_policy test {
 }
 
 
-# 
+#
 #  create role policy
-# 
+#
 
-resource keycloak_role test {
-  realm_id = "${keycloak_realm_authorization.test.id}"
+resource keycloak_role test_authorization2 {
+  realm_id = "${keycloak_realm.test_authorization.id}"
   name     = "new_role"
 }
 
-resource keycloak_openid_client_authorization_role_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
-  name               = "keycloak_openid_client_authorization_role_policy"
+resource keycloak_openid_client_role_policy test1 {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
+  name               = "keycloak_openid_client_role_policy1"
   decision_strategy  = "AFFIRMATIVE"
   logic              = "POSITIVE"
   type               = "role"
   role {
-    id       = "${keycloak_role.test.id}"
+    id       = "${keycloak_role.test_authorization2.id}"
     required = false
   }
 }
 
-# 
+#
 # create time policy
-# 
+#
 
-resource keycloak_openid_client_authorization_time_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
+resource keycloak_openid_client_time_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
   name               = "%s"
   not_on_or_after    = "2500-12-12 01:01:11"
   not_before         = "2400-12-12 01:01:11"
@@ -151,12 +151,12 @@ resource keycloak_openid_client_authorization_time_policy test {
   decision_strategy  = "UNANIMOUS"
 }
 
-# 
+#
 # create user policy
-# 
+#
 
 resource keycloak_user test {
-  realm_id = "${keycloak_realm_authorization.test.id}"
+  realm_id = "${keycloak_realm.test_authorization.id}"
   username = "test-user"
 
   email      = "test-user@fakedomain.com"
@@ -164,9 +164,9 @@ resource keycloak_user test {
   last_name  = "Tester"
 }
 
-resource keycloak_openid_client_authorization_user_policy test {
-  resource_server_id = "${keycloak_openid_client_authorization.test.resource_server_id}"
-  realm_id           = "${keycloak_realm_authorization.test.id}"
+resource keycloak_openid_client_user_policy test {
+  resource_server_id = "${keycloak_openid_client.test.resource_server_id}"
+  realm_id           = "${keycloak_realm.test_authorization.id}"
   name               = "client_user_policy_test"
   users              = ["${keycloak_user.test.id}"]
   logic              = "POSITIVE"
