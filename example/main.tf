8d9c31e1f013efcdbd0502c70145b03449c271d6
Damien Le Chevalier <damien.lechevalier@ext.cdiscount.com>
2020-05-26 14:50:12 +0200

    add userClientRole protocol mapper
    Fix doc

--- example/main.tf
+++ example/main.tf
@@ -395,6 +395,40 @@ resource "keycloak_openid_user_realm_role_protocol_mapper" "user_realm_role_clie
   claim_name = "foo"
 }
 
+resource "keycloak_openid_user_client_role_protocol_mapper" "user_client_role_client" {
+  name      = "tf-test-open-id-user-client-role-claim-protocol-mapper-client"
+  realm_id  = "${keycloak_realm.test.id}"
+  client_id = "${keycloak_openid_client.test_client.id}"
+
+  claim_name = "foo"
+  claim_value = "bar"
+  multivalued = false
+
+  client_id_for_role_mappings   = "${keycloak_openid_client.bearer_only_client.client_id}"
+  client_role_prefix = "prefixValue"
+
+  add_to_id_token     = true
+  add_to_access_token = false
+  add_to_user_info = false
+}
+
+resource "keycloak_openid_user_client_role_protocol_mapper" "user_client_role_client_scope" {
+  name            = "tf-test-open-id-user-client-role-protocol-mapper-client-scope"
+  realm_id        = "${keycloak_realm.test.id}"
+  client_scope_id = "${keycloak_openid_client_scope.test_default_client_scope.id}"
+
+  claim_name  = "foo"
+  claim_value = "bar"
+  multivalued = false
+
+  client_id_for_role_mappings   = "${keycloak_openid_client.bearer_only_client.client_id}"
+  client_role_prefix = "prefixValue"
+
+  add_to_id_token     = true
+  add_to_access_token = false
+  add_to_user_info    = false
+}
+
 resource "keycloak_openid_client" "bearer_only_client" {
   client_id   = "test-bearer-only-client"
   name        = "test-bearer-only-client"
