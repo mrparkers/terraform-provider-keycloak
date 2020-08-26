4d935b901e410c0003c50754b6106be209a4719e
Michael Parker <michael@parker.gg>
2020-08-26 13:20:07 -0500

    remove override of provider configurecontextfunc for password grant test

--- provider/provider_password_grant_test.go
+++ provider/provider_password_grant_test.go
@@ -1,8 +1,6 @@
 package provider
 
 import (
-	"context"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -22,11 +20,6 @@ func TestAccKeycloakProvider_passwordGrant(t *testing.T) {
 	}()
 
 	provider := KeycloakProvider()
-	providerConfigureFunc := provider.ConfigureContextFunc
-	// override the default context so a failing test doesn't cancel other tests
-	provider.ConfigureContextFunc = func(_ context.Context, data *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		return providerConfigureFunc(context.Background(), data)
-	}
 
 	realmName := "terraform-" + acctest.RandString(10)
 	clientId := "terraform-" + acctest.RandString(10)
