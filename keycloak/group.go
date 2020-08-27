124201a59a0e740bca4a1236e557fa2ccf1ea9a8
Jerome Marchand <prestataire.0121541@ouest-france.fr>
2020-08-27 16:30:08 +0200

    Fix group import

--- keycloak/group.go
+++ keycloak/group.go
@@ -35,27 +35,27 @@ func (keycloakClient *KeycloakClient) groupParentId(group *Group) (string, error
 	}
 
 	var parentGroup Group
-	if findParentGroup(*group, groups, parentGroup) {
-		return parentGroup.Id, nil
+	if parentGroupId, found := findParentGroup(*group, groups, parentGroup); found {
+		return parentGroupId, nil
 	}
 
 	// maybe panic here?  this should never happen
 	return "", fmt.Errorf("unable to determine parent ID for group with path %s", group.Path)
 }
 
-func findParentGroup(group Group, ingroups []*Group, parentGroup Group) bool {
+func findParentGroup(group Group, ingroups []*Group, parentGroup Group) (string, bool) {
 	for _, grp := range ingroups {
 		if grp.Id == group.Id {
-			return true
+			return parentGroup.Id, true
 		}
 		if strings.HasPrefix(group.Path, grp.Path+"/") {
-			parentGroup = *grp
-			if findParentGroup(group, grp.SubGroups, parentGroup) {
-				return true
+
+			if parentGroupId, found := findParentGroup(group, grp.SubGroups, *grp); found {
+				return parentGroupId, found
 			}
 		}
 	}
-	return false
+	return "", false
 }
 
 func (keycloakClient *KeycloakClient) ValidateGroupMembers(usernames []interface{}) error {
