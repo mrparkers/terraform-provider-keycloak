66c5c13edab0b1aec67dd381ef5e3e18323af934
pmellati <pouria.mellati@xinja.com.au>
2020-09-03 16:14:36 +1000

    Add offline_session_max_lifespan_enabled attribute to realm.

--- provider/data_source_keycloak_realm.go
+++ provider/data_source_keycloak_realm.go
@@ -196,6 +196,10 @@ func dataSourceKeycloakRealm() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"offline_session_max_lifespan_enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 			"access_token_lifespan": {
 				Type:     schema.TypeString,
 				Computed: true,
