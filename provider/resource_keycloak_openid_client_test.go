681c97ebf44ea05f880371a0301b5e2fdb1923dc
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-09-23 11:47:17 +0200

    Cleanup

--- provider/resource_keycloak_openid_client_test.go
+++ provider/resource_keycloak_openid_client_test.go
@@ -478,7 +478,7 @@ func TestAccKeycloakOpenidClient_otherAttributes(t *testing.T) {
 				Check:  testAccCheckKeycloakOpenidClientHasAttributeWithValue("keycloak_openid_client.client", "foo", "bar"),
 			},
 			{
-				Config:      testKeycloakOpenidClient_otherAttributes(realmName, clientId, "pkce.code.challenge.method'", "bar"),
+				Config:      testKeycloakOpenidClient_otherAttributes(realmName, clientId, "pkce.code.challenge.method", "bar"),
 				ExpectError: regexp.MustCompile("pkce.code.challenge.method is a wrong key in attributes. Use the field defined for this purpose instead."),
 			},
 		},
