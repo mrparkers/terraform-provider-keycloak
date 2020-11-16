a852a54e914f60332829832a377f0908f826518a
Jérôme MARCHAND <jerome.marchand@zenika.com>
2020-11-16 16:38:26 +0100

    new resource: keycloak_users_permissions (#400)

---
+++ keycloak/users_permissions.go
@@ -0,0 +1,37 @@
+package keycloak
+
+import (
+	"fmt"
+)
+
+type UsersPermissionsInput struct {
+	Enabled bool `json:"enabled"`
+}
+
+type UsersPermissions struct {
+	RealmId          string                 `json:"-"`
+	Enabled          bool                   `json:"enabled"`
+	Resource         string                 `json:"resource"`
+	ScopePermissions map[string]interface{} `json:"scopePermissions"`
+}
+
+func (keycloakClient *KeycloakClient) EnableUsersPermissions(realmId string) error {
+	return keycloakClient.put(fmt.Sprintf("/realms/%s/users-management-permissions", realmId), UsersPermissionsInput{Enabled: true})
+}
+
+func (keycloakClient *KeycloakClient) DisableUsersPermissions(realmId string) error {
+	return keycloakClient.put(fmt.Sprintf("/realms/%s/users-management-permissions", realmId), UsersPermissionsInput{Enabled: false})
+}
+
+func (keycloakClient *KeycloakClient) GetUsersPermissions(realmId string) (*UsersPermissions, error) {
+	var openidClientPermissions UsersPermissions
+
+	err := keycloakClient.get(fmt.Sprintf("/realms/%s/users-management-permissions", realmId), &openidClientPermissions, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	openidClientPermissions.RealmId = realmId
+
+	return &openidClientPermissions, nil
+}
