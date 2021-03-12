2df0fac6774c46b4c4ec79d497bdd38a74f4c85c
David MEJIA <Zeldhyr@users.noreply.github.com>
2021-03-12 17:30:14 +0100

    new data source: keycloak_authentication_flow (#486)

--- keycloak/authentication_flow.go
+++ keycloak/authentication_flow.go
@@ -2,6 +2,7 @@ package keycloak
 
 import (
 	"fmt"
+	"time"
 )
 
 type AuthenticationFlow struct {
@@ -51,6 +52,50 @@ func (keycloakClient *KeycloakClient) GetAuthenticationFlow(realmId, id string) 
 
 	authenticationFlow.RealmId = realmId
 	return &authenticationFlow, nil
+}
+
+func (keycloakClient *KeycloakClient) GetAuthenticationFlowFromAlias(realmId, alias string) (*AuthenticationFlow, error) {
+	var authenticationFlows []*AuthenticationFlow
+	var authenticationFlow *AuthenticationFlow = nil
+
+	err := keycloakClient.get(fmt.Sprintf("/realms/%s/authentication/flows", realmId), &authenticationFlows, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Retry 3 more times if not found, sometimes it took split milliseconds the Authentication to populate
+	if len(authenticationFlows) == 0 {
+		for i := 0; i < 3; i++ {
+			err := keycloakClient.get(fmt.Sprintf("/realms/%s/authentication/flows", realmId), &authenticationFlows, nil)
+
+			if len(authenticationFlows) > 0 {
+				break
+			}
+
+			if err != nil {
+				return nil, err
+			}
+
+			time.Sleep(time.Millisecond * 50)
+		}
+
+		if len(authenticationFlows) == 0 {
+			return nil, fmt.Errorf("no authentication flow found for alias %s", alias)
+		}
+	}
+
+	for _, authFlow := range authenticationFlows {
+		if authFlow.Alias == alias {
+			authenticationFlow = authFlow
+		}
+	}
+
+	if authenticationFlow == nil {
+		return nil, fmt.Errorf("no authentication flow found for alias %s", alias)
+	}
+	authenticationFlow.RealmId = realmId
+
+	return authenticationFlow, nil
 }
 
 func (keycloakClient *KeycloakClient) UpdateAuthenticationFlow(authenticationFlow *AuthenticationFlow) error {
