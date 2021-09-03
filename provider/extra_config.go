dce16eb5abe25050c9e899e51f57f71056c55848
Michael Parker <michael@parker.gg>
2021-09-03 13:04:45 -0500

    more comments for extra_config logic

--- provider/extra_config.go
+++ provider/extra_config.go
@@ -11,6 +11,9 @@ func getExtraConfigFromData(data *schema.ResourceData) map[string]interface{} {
 			extraConfig[key] = value
 		}
 
+		// check if extra config attribute has been deleted.
+		// it's not enough to simply unset the attribute - we have to explicitly set
+		// it to empty string in order to remove this on the Keycloak side
 		if data.HasChange("extra_config") && !data.IsNewResource() {
 			oldConfig, newConfig := data.GetChange("extra_config")
 			newConfigMap := newConfig.(map[string]interface{})
@@ -29,6 +32,7 @@ func getExtraConfigFromData(data *schema.ResourceData) map[string]interface{} {
 func setExtraConfigData(data *schema.ResourceData, extraConfig map[string]interface{}) {
 	c := map[string]interface{}{}
 
+	// when saving back to state, don't persist empty attributes that we're trying to remove from Keycloak
 	for k, v := range extraConfig {
 		if s, ok := v.(string); ok && s == "" {
 			continue
