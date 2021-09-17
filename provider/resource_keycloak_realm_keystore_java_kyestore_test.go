0cc99c4300a771646fbeb6a0f88f23c1595337fe
Vlad Kirichenko <v.kirichenko@napoleongames.be>
2021-09-17 10:55:33 +0300

    Added java keystore test
    Signed-off-by: Vlad Kirichenko <v.kirichenko@napoleongames.be>

--- provider/resource_keycloak_realm_keystore_java_kyestore_test.go
+++ provider/resource_keycloak_realm_keystore_java_kyestore_test.go
@@ -1,257 +1,270 @@
 package provider
 
-//import (
-//	"fmt"
-//	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-//	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-//	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-//	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
-//	"regexp"
-//	"strconv"
-//	"testing"
-//)
-//
-//func TestAccKeycloakRealmKeystoreJavaKeystoreGenerated_basic(t *testing.T) {
-//	t.Parallel()
-//
-//	javaKeystoreName := acctest.RandomWithPrefix("tf-acc")
-//
-//	resource.Test(t, resource.TestCase{
-//		ProviderFactories: testAccProviderFactories,
-//		PreCheck:          func() { testAccPreCheck(t) },
-//		CheckDestroy:      testAccCheckRealmKeystoreJavaKeystoreGeneratedDestroy(),
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basic(javaKeystoreName),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedExists("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore"),
-//			},
-//			{
-//				ResourceName:      "keycloak_realm_key_javaKeystore_generated.realm_javaKeystore",
-//				ImportState:       true,
-//				ImportStateVerify: true,
-//				ImportStateIdFunc: getRealmKeystoreJavaKeystoreGeneratedImportId("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore"),
-//			},
-//		},
-//	})
-//}
-//
-//func TestAccKeycloakRealmKeystoreJavaKeystoreGenerated_createAfterManualDestroy(t *testing.T) {
-//	t.Parallel()
-//
-//	var javaKeystore = &keycloak.RealmKeystoreJavaKeystore{}
-//
-//	fullNameMapperName := acctest.RandomWithPrefix("tf-acc")
-//
-//	resource.Test(t, resource.TestCase{
-//		ProviderFactories: testAccProviderFactories,
-//		PreCheck:          func() { testAccPreCheck(t) },
-//		CheckDestroy:      testAccCheckRealmKeystoreJavaKeystoreGeneratedDestroy(),
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basic(fullNameMapperName),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedFetch("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore", javaKeystore),
-//			},
-//			{
-//				PreConfig: func() {
-//					err := keycloakClient.DeleteRealmKeystoreJavaKeystore(javaKeystore.RealmId, javaKeystore.Id)
-//					if err != nil {
-//						t.Fatal(err)
-//					}
-//				},
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basic(fullNameMapperName),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedFetch("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore", javaKeystore),
-//			},
-//		},
-//	})
-//}
-//
-//func TestAccKeycloakRealmKeystoreJavaKeystoreGenerated_ellipticCurveValidation(t *testing.T) {
-//	t.Parallel()
-//
-//	javaKeystoreName := acctest.RandomWithPrefix("tf-acc")
-//	ellipticCurve := randomStringInSlice(keycloakRealmKeystoreJavaKeystoreGeneratedEllipticCurve)
-//
-//	resource.Test(t, resource.TestCase{
-//		ProviderFactories: testAccProviderFactories,
-//		PreCheck:          func() { testAccPreCheck(t) },
-//		CheckDestroy:      testAccCheckRealmKeystoreJavaKeystoreGeneratedDestroy(),
-//		Steps: []resource.TestStep{
-//			{
-//				Config:      testKeycloakRealmKeystoreJavaKeystoreGenerated_basicWithAttrValidation(javaKeystoreName, "elliptic_curve_key", acctest.RandString(10)),
-//				ExpectError: regexp.MustCompile("expected mode to be one of .+ got .+"),
-//			},
-//			{
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basicWithAttrValidation(javaKeystoreName, "elliptic_curve_key", ellipticCurve),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedExists("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore"),
-//			},
-//		},
-//	})
-//}
-//
-//func TestAccKeycloakRealmKeystoreJavaKeystoreGenerated_updateLdapUserFederationInPlace(t *testing.T) {
-//	t.Parallel()
-//
-//	enabled := randomBool()
-//	active := randomBool()
-//
-//	groupMapperOne := &keycloak.RealmKeystoreJavaKeystore{
-//		Name:     acctest.RandString(10),
-//		RealmId:  testAccRealmUserFederation.Realm,
-//		Enabled:  enabled,
-//		Active:   active,
-//		Priority: acctest.RandInt(),
-//	}
-//
-//	groupMapperTwo := &keycloak.RealmKeystoreJavaKeystore{
-//		Name:     acctest.RandString(10),
-//		RealmId:  testAccRealmUserFederation.Realm,
-//		Enabled:  enabled,
-//		Active:   active,
-//		Priority: acctest.RandInt(),
-//	}
-//
-//	resource.Test(t, resource.TestCase{
-//		ProviderFactories: testAccProviderFactories,
-//		PreCheck:          func() { testAccPreCheck(t) },
-//		CheckDestroy:      testAccCheckRealmKeystoreJavaKeystoreGeneratedDestroy(),
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basicFromInterface(groupMapperOne),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedExists("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore"),
-//			},
-//			{
-//				Config: testKeycloakRealmKeystoreJavaKeystoreGenerated_basicFromInterface(groupMapperTwo),
-//				Check:  testAccCheckRealmKeystoreJavaKeystoreGeneratedExists("keycloak_realm_key_javaKeystore_generated.realm_javaKeystore"),
-//			},
-//		},
-//	})
-//}
-//
-//func testAccCheckRealmKeystoreJavaKeystoreGeneratedExists(resourceName string) resource.TestCheckFunc {
-//	return func(s *terraform.State) error {
-//		_, err := getKeycloakRealmKeystoreJavaKeystoreGeneratedFromState(s, resourceName)
-//		if err != nil {
-//			return err
-//		}
-//
-//		return nil
-//	}
-//}
-//
-//func testAccCheckRealmKeystoreJavaKeystoreGeneratedFetch(resourceName string, mapper *keycloak.RealmKeystoreJavaKeystoreGenerated) resource.TestCheckFunc {
-//	return func(s *terraform.State) error {
-//		fetchedMapper, err := getKeycloakRealmKeystoreJavaKeystoreGeneratedFromState(s, resourceName)
-//		if err != nil {
-//			return err
-//		}
-//
-//		mapper.Id = fetchedMapper.Id
-//		mapper.RealmId = fetchedMapper.RealmId
-//
-//		return nil
-//	}
-//}
-//
-//func testAccCheckRealmKeystoreJavaKeystoreGeneratedDestroy() resource.TestCheckFunc {
-//	return func(s *terraform.State) error {
-//		for _, rs := range s.RootModule().Resources {
-//			if rs.Type != "keycloak_realm_key_javaKeystore_generated" {
-//				continue
-//			}
-//
-//			id := rs.Primary.ID
-//			realm := rs.Primary.Attributes["realm_id"]
-//
-//			ldapGroupMapper, _ := keycloakClient.GetRealmKeystoreJavaKeystoreGenerated(realm, id)
-//			if ldapGroupMapper != nil {
-//				return fmt.Errorf("javaKeystore keystore with id %s still exists", id)
-//			}
-//		}
-//
-//		return nil
-//	}
-//}
-//
-//func getKeycloakRealmKeystoreJavaKeystoreGeneratedFromState(s *terraform.State,
-//	resourceName string) (*keycloak.RealmKeystoreJavaKeystoreGenerated,
-//	error) {
-//	rs, ok := s.RootModule().Resources[resourceName]
-//	if !ok {
-//		return nil, fmt.Errorf("resource not found: %s", resourceName)
-//	}
-//
-//	id := rs.Primary.ID
-//	realm := rs.Primary.Attributes["realm_id"]
-//
-//	realmKeystore, err := keycloakClient.GetRealmKeystoreJavaKeystoreGenerated(realm, id)
-//	if err != nil {
-//		return nil, fmt.Errorf("error getting javaKeystore keystore with id %s: %s", id, err)
-//	}
-//
-//	return realmKeystore, nil
-//}
-//
-//func getRealmKeystoreJavaKeystoreGeneratedImportId(resourceName string) resource.ImportStateIdFunc {
-//	return func(s *terraform.State) (string, error) {
-//		rs, ok := s.RootModule().Resources[resourceName]
-//		if !ok {
-//			return "", fmt.Errorf("resource not found: %s", resourceName)
-//		}
-//
-//		id := rs.Primary.ID
-//		realmId := rs.Primary.Attributes["realm_id"]
-//		providerId := "java-keystore"
-//
-//		return fmt.Sprintf("%s/%s/%s", realmId, providerId, id), nil
-//	}
-//}
-//
-//func testKeycloakRealmKeystoreJavaKeystoreGenerated_basic(javaKeystoreName string) string {
-//	return fmt.Sprintf(`
-//data "keycloak_realm" "realm" {
-//	realm = "%s"
-//}
-//
-//resource "keycloak_realm_key_javaKeystore_generated" "realm_java_keystore" {
-//	name      = "%s"
-//	realm_id  = data.keycloak_realm.realm.id
-//	parent_id = data.keycloak_realm.realm.id
-//
-//   priority           = 100
-//   elliptic_curve_key = "P-384"
-//}
-//	`, testAccRealmUserFederation.Realm, javaKeystoreName)
-//}
-//
-//func testKeycloakRealmKeystoreJavaKeystoreGenerated_basicWithAttrValidation(javaKeystoreName, attr, val string) string {
-//	return fmt.Sprintf(`
-//data "keycloak_realm" "realm" {
-//	realm = "%s"
-//}
-//
-//resource "keycloak_ldap_group_mapper" "realm_java_keystore" {
-//	name      = "%s"
-//	realm_id  = data.keycloak_realm.realm.id
-//	parent_id = data.keycloak_realm.realm.id
-//
-//	%s         = "%s"
-//}
-//	`, testAccRealmUserFederation.Realm, javaKeystoreName, attr, val)
-//}
-//
-//func testKeycloakRealmKeystoreJavaKeystoreGenerated_basicFromInterface(mapper *keycloak.RealmKeystoreJavaKeystore) string {
-//	return fmt.Sprintf(`
-//data "keycloak_realm" "realm" {
-//	realm = "%s"
-//}
-//
-//resource "keycloak_ldap_group_mapper" "realm_java_keystore" {
-//	name      = "%s"
-//	realm_id  = data.keycloak_realm.realm.id
-//	parent_id = data.keycloak_realm.realm.id
-//
-//   priority           = "%s"
-//   elliptic_curve_key = "%s"
-//}
-//	`, testAccRealmUserFederation.Realm, mapper.Name, strconv.Itoa(mapper.Priority), mapper.EllipticCurve)
-//}
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
+	"regexp"
+	"strconv"
+	"testing"
+)
+
+func TestAccKeycloakRealmKeystoreJava_basic(t *testing.T) {
+	t.Parallel()
+
+	javaKeystoreName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckRealmKeystoreJavaDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRealmKeystoreJava_basic(javaKeystoreName),
+				Check:  testAccCheckRealmKeystoreJavaExists("keycloak_realm_key_java_keystore.realm_java_keystore"),
+			},
+		},
+	})
+}
+
+func TestAccKeycloakRealmKeystoreJava_createAfterManualDestroy(t *testing.T) {
+	t.Parallel()
+
+	var javaKeystore = &keycloak.RealmKeystoreJavaKeystore{}
+
+	fullNameKeystoreName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckRealmKeystoreJavaDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRealmKeystoreJava_basic(fullNameKeystoreName),
+				Check:  testAccCheckRealmKeystoreJavaFetch("keycloak_realm_key_java_keystore.realm_java_keystore", javaKeystore),
+			},
+			{
+				PreConfig: func() {
+					err := keycloakClient.DeleteRealmKeystoreJavaKeystore(javaKeystore.RealmId, javaKeystore.Id)
+					if err != nil {
+						t.Fatal(err)
+					}
+				},
+				Config: testKeycloakRealmKeystoreJava_basic(fullNameKeystoreName),
+				Check:  testAccCheckRealmKeystoreJavaFetch("keycloak_realm_key_java_keystore.realm_java_keystore", javaKeystore),
+			},
+		},
+	})
+}
+
+func TestAccKeycloakRealmKeystoreJava_algorithmValidation(t *testing.T) {
+	t.Parallel()
+
+	algorithm := randomStringInSlice(keycloakRealmKeystoreRsaAlgorithm)
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckRealmKeystoreJavaDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRealmKeystoreJava_basicWithAttrValidation(algorithm, "algorithm",
+					acctest.RandString(10)),
+				ExpectError: regexp.MustCompile("expected algorithm to be one of .+ got .+"),
+			},
+			{
+				Config: testKeycloakRealmKeystoreJava_basicWithAttrValidation(algorithm, "algorithm", algorithm),
+				Check:  testAccCheckRealmKeystoreJavaExists("keycloak_realm_key_java_keystore.realm_java_keystore"),
+			},
+		},
+	})
+}
+
+func TestAccKeycloakRealmKeystoreJava_updateRsaKeystoreGenerated(t *testing.T) {
+	t.Parallel()
+
+	enabled := randomBool()
+	active := randomBool()
+
+	groupKeystoreOne := &keycloak.RealmKeystoreJavaKeystore{
+		Name:      acctest.RandString(10),
+		RealmId:   testAccRealmUserFederation.Realm,
+		Enabled:   enabled,
+		Active:    active,
+		Priority:  acctest.RandIntRange(0, 100),
+		Algorithm: randomStringInSlice(keycloakRealmKeystoreJavaKeystoreAlgorithm),
+	}
+
+	groupKeystoreTwo := &keycloak.RealmKeystoreJavaKeystore{
+		Name:      acctest.RandString(10),
+		RealmId:   testAccRealmUserFederation.Realm,
+		Enabled:   enabled,
+		Active:    active,
+		Priority:  acctest.RandIntRange(0, 100),
+		Algorithm: randomStringInSlice(keycloakRealmKeystoreJavaKeystoreAlgorithm),
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckRealmKeystoreJavaDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakRealmKeystoreJava_basicFromInterface(groupKeystoreOne),
+				Check:  testAccCheckRealmKeystoreJavaExists("keycloak_realm_key_java_keystore.realm_java_keystore"),
+			},
+			{
+				Config: testKeycloakRealmKeystoreJava_basicFromInterface(groupKeystoreTwo),
+				Check:  testAccCheckRealmKeystoreJavaExists("keycloak_realm_key_java_keystore.realm_java_keystore"),
+			},
+		},
+	})
+}
+
+func testAccCheckRealmKeystoreJavaExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, err := getKeycloakRealmKeystoreJavaFromState(s, resourceName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckRealmKeystoreJavaFetch(resourceName string, keystore *keycloak.RealmKeystoreJavaKeystore) resource.
+	TestCheckFunc {
+	return func(s *terraform.State) error {
+		fetchedKeystore, err := getKeycloakRealmKeystoreJavaFromState(s, resourceName)
+		if err != nil {
+			return err
+		}
+
+		keystore.Id = fetchedKeystore.Id
+		keystore.RealmId = fetchedKeystore.RealmId
+
+		return nil
+	}
+}
+
+func testAccCheckRealmKeystoreJavaDestroy() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "keycloak_realm_key_java_keystore" {
+				continue
+			}
+
+			id := rs.Primary.ID
+			realm := rs.Primary.Attributes["realm_id"]
+
+			ldapGroupKeystore, _ := keycloakClient.GetRealmKeystoreJavaKeystore(realm, id)
+			if ldapGroupKeystore != nil {
+				return fmt.Errorf("rsa keystore with id %s still exists", id)
+			}
+		}
+
+		return nil
+	}
+}
+
+func getKeycloakRealmKeystoreJavaFromState(s *terraform.State,
+	resourceName string) (*keycloak.RealmKeystoreJavaKeystore,
+	error) {
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return nil, fmt.Errorf("resource not found: %s", resourceName)
+	}
+
+	id := rs.Primary.ID
+	realm := rs.Primary.Attributes["realm_id"]
+
+	realmKeystore, err := keycloakClient.GetRealmKeystoreJavaKeystore(realm, id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting rsa keystore with id %s: %s", id, err)
+	}
+
+	return realmKeystore, nil
+}
+
+func getRealmKeystoreJavaImportId(resourceName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("resource not found: %s", resourceName)
+		}
+
+		id := rs.Primary.ID
+		realmId := rs.Primary.Attributes["realm_id"]
+		providerId := "rsa-generated"
+
+		return fmt.Sprintf("%s/%s/%s", realmId, providerId, id), nil
+	}
+}
+
+func testKeycloakRealmKeystoreJava_basic(javaKeystoreName string) string {
+	return fmt.Sprintf(`
+data "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_realm_key_java_keystore" "realm_java_keystore" {
+
+	name      = "%s"
+	realm_id  = data.keycloak_realm.realm.id
+	parent_id = data.keycloak_realm.realm.id
+
+    keystore          = "/go/src/github.com/mrparkers/terraform-provider-keycloak/provider/misc/java-keystore.jks"
+    keystore_password = "12345678"
+    key_alias         = "test"
+    key_password      = "12345678"
+
+    priority  = 100
+    algorithm = "RS256"
+}
+	`, testAccRealmUserFederation.Realm, javaKeystoreName)
+}
+
+func testKeycloakRealmKeystoreJava_basicWithAttrValidation(javaKeystoreName, attr, val string) string {
+	return fmt.Sprintf(`
+data "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_realm_key_java_keystore" "realm_java_keystore" {
+	name      = "%s"
+	realm_id  = data.keycloak_realm.realm.id
+	parent_id = data.keycloak_realm.realm.id
+
+    keystore          = "/go/src/github.com/mrparkers/terraform-provider-keycloak/provider/misc/java-keystore.jks"
+    keystore_password = "12345678"
+    key_alias         = "test"
+    key_password      = "12345678"
+
+	%s        = "%s"
+}
+	`, testAccRealmUserFederation.Realm, javaKeystoreName, attr, val)
+}
+
+func testKeycloakRealmKeystoreJava_basicFromInterface(keystore *keycloak.RealmKeystoreJavaKeystore) string {
+	return fmt.Sprintf(`
+data "keycloak_realm" "realm" {
+	realm = "%s"
+}
+
+resource "keycloak_realm_key_java_keystore" "realm_java_keystore" {
+	name      = "%s"
+	realm_id  = data.keycloak_realm.realm.id
+	parent_id = data.keycloak_realm.realm.id
+
+    keystore          = "/go/src/github.com/mrparkers/terraform-provider-keycloak/provider/misc/java-keystore.jks"
+    keystore_password = "12345678"
+    key_alias         = "test"
+    key_password      = "12345678"
+
+    priority  = %s
+    algorithm = "%s"
+}
+	`, testAccRealmUserFederation.Realm, keystore.Name, strconv.Itoa(keystore.Priority), keystore.Algorithm)
+}
