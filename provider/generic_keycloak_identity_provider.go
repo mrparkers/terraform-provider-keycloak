678b865c7789463f56b37e68ac3a1e0d8bec7285
dnic <dnic@appdat.jsc.nasa.gov>
2022-07-05 13:24:36 -0600

    Add diag import

--- provider/generic_keycloak_identity_provider.go
+++ provider/generic_keycloak_identity_provider.go
@@ -3,12 +3,12 @@ package provider
 import (
 	"context"
 	"fmt"
-	"reflect"
-	"strings"
-
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
+	"reflect"
+	"strings"
 )
 
 var syncModes = []string{
