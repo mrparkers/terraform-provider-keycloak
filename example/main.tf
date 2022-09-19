ec4fddd2e361dd7ca863c1b49e9b1feb482c8810
Michael Parker <michael@parker.gg>
2022-09-19 10:59:58 -0500

    chore: more changes to update default to quarkus

--- example/main.tf
+++ example/main.tf
@@ -11,6 +11,7 @@ provider "keycloak" {
   client_id          = "terraform"
   client_secret      = "884e0f95-0f42-4a63-9b1f-94274655669e"
   url                = "http://localhost:8080"
+  base_path          = ""
   additional_headers = {
     foo = "bar"
   }
