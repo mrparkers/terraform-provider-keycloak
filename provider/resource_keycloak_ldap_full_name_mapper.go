82baf88b961c2902f61b0597e1e0ef45c92ab658
Michael Parker <michael@parker.gg>
2022-10-06 14:18:27 -0500

    fix: use correct ID when creating full name mapper

--- provider/resource_keycloak_ldap_full_name_mapper.go
+++ provider/resource_keycloak_ldap_full_name_mapper.go
@@ -57,7 +57,7 @@ func resourceKeycloakLdapFullNameMapper() *schema.Resource {
 
 func getLdapFullNameMapperFromData(data *schema.ResourceData) *keycloak.LdapFullNameMapper {
 	return &keycloak.LdapFullNameMapper{
-		Id:                   data.Get("id").(string),
+		Id:                   data.Id(),
 		Name:                 data.Get("name").(string),
 		RealmId:              data.Get("realm_id").(string),
 		LdapUserFederationId: data.Get("ldap_user_federation_id").(string),
