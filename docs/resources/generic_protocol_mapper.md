10e59625b45741464ca3c34a3fba07011981e6a5
Michael Parker <michael@parker.gg>
2022-10-10 08:35:08 -0500

    feat: rename keycloak_generic_client_role_mapper to keycloak_generic_role_mapper (#748)

--- docs/resources/generic_protocol_mapper.md
+++ docs/resources/generic_protocol_mapper.md
@@ -1,10 +1,8 @@
 ---
-page_title: "keycloak_generic_client_protocol_mapper Resource"
+page_title: "keycloak_generic_protocol_mapper Resource"
 ---
 
-# keycloak\_generic\_client\_protocol\_mapper Resource
-
-!> **WARNING:** This resource is deprecated and will be removed in the next major version. Please use `keycloak_generic_protocol_mapper` instead.
+# keycloak\_generic\_protocol\_mapper Resource
 
 Allows for creating and managing protocol mappers for both types of clients (openid-connect and saml) within Keycloak.
 
@@ -13,7 +11,7 @@ There are two uses cases for using this resource:
 * If the provider doesn't support a particular protocol mapper, this resource can be used instead.
 
 Due to the generic nature of this mapper, it is less user-friendly and more prone to configuration errors.
-Therefore, if possible, a specific mapper should be used.
+Therefore, if possible, a specific mapper should be used instead.
 
 ## Example Usage
 
@@ -28,7 +26,7 @@ resource "keycloak_saml_client" "saml_client" {
   client_id = "test-client"
 }
 
-resource "keycloak_generic_client_protocol_mapper" "saml_hardcode_attribute_mapper" {
+resource "keycloak_generic_protocol_mapper" "saml_hardcode_attribute_mapper" {
   realm_id        = keycloak_realm.realm.id
   client_id       = keycloak_saml_client.saml_client.id
   name            = "test-mapper"
@@ -46,10 +44,11 @@ resource "keycloak_generic_client_protocol_mapper" "saml_hardcode_attribute_mapp
 ## Argument Reference
 
 - `realm_id` - (Required) The realm this protocol mapper exists within.
-- `client_id` - (Required) The client this protocol mapper is attached to.
 - `name` - (Required) The display name of this protocol mapper in the GUI.
 - `protocol` - (Required) The type of client (either `openid-connect` or `saml`). The type must match the type of the client.
 - `protocol_mapper` - (Required) The name of the protocol mapper. The protocol mapper must be compatible with the specified client.
+- `client_id` - (Optional) The ID of the client this protocol mapper should be added to. Conflicts with `client_scope_id`. This argument is required if `client_scope_id` is not set.
+- `client_scope_id` - (Optional) The ID of the client scope this protocol mapper should be added to. Conflicts with `client_id`. This argument is required if `client_id` is not set.
 - `config` - (Required) A map with key / value pairs for configuring the protocol mapper. The supported keys depends on the protocol mapper.
 
 ## Import
@@ -59,5 +58,5 @@ Protocol mappers can be imported using the following format: `{{realm_id}}/clien
 Example:
 
 ```bash
-$ terraform import keycloak_generic_client_protocol_mapper.saml_hardcode_attribute_mapper my-realm/client/a7202154-8793-4656-b655-1dd18c181e14/71602afa-f7d1-4788-8c49-ef8fd00af0f4
+$ terraform import keycloak_generic_protocol_mapper.saml_hardcode_attribute_mapper my-realm/client/a7202154-8793-4656-b655-1dd18c181e14/71602afa-f7d1-4788-8c49-ef8fd00af0f4
 ```
---
+++ provider/resource_keycloak_generic_role_mapper.go
@@ -0,0 +1,139 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
+)
+
+func resourceKeycloakGenericRoleMapper() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceKeycloakGenericRoleMapperCreate,
+		ReadContext:   resourceKeycloakGenericRoleMapperRead,
+		DeleteContext: resourceKeycloakGenericRoleMapperDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceKeycloakGenericRoleMapperImport,
+		},
+		Schema: map[string]*schema.Schema{
+			"realm_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The realm id where the associated client or client scope exists.",
+			},
+			"client_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				Description:   "The destination client of the role. Cannot be used at the same time as client_scope_id.",
+				ConflictsWith: []string{"client_scope_id"},
+			},
+			"client_scope_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				Description:   "The destination client scope of the role. Cannot be used at the same time as client_id.",
+				ConflictsWith: []string{"client_id"},
+			},
+			"role_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Id of the role to assign",
+			},
+		},
+	}
+}
+
+func resourceKeycloakGenericRoleMapperCreate(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	keycloakClient := meta.(*keycloak.KeycloakClient)
+
+	realmId := data.Get("realm_id").(string)
+	clientId := data.Get("client_id").(string)
+	clientScopeId := data.Get("client_scope_id").(string)
+	roleId := data.Get("role_id").(string)
+
+	role, err := keycloakClient.GetRole(ctx, realmId, roleId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = keycloakClient.CreateRoleScopeMapping(ctx, realmId, clientId, clientScopeId, role)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if clientId != "" {
+		data.SetId(fmt.Sprintf("%s/client/%s/scope-mappings/%s/%s", realmId, clientId, role.ClientId, role.Id))
+	} else {
+		data.SetId(fmt.Sprintf("%s/client-scope/%s/scope-mappings/%s/%s", realmId, clientScopeId, role.ClientId, role.Id))
+	}
+
+	return resourceKeycloakGenericRoleMapperRead(ctx, data, meta)
+}
+
+func resourceKeycloakGenericRoleMapperRead(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	keycloakClient := meta.(*keycloak.KeycloakClient)
+
+	realmId := data.Get("realm_id").(string)
+	clientId := data.Get("client_id").(string)
+	clientScopeId := data.Get("client_scope_id").(string)
+	roleId := data.Get("role_id").(string)
+
+	role, err := keycloakClient.GetRole(ctx, realmId, roleId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	mappedRole, err := keycloakClient.GetRoleScopeMapping(ctx, realmId, clientId, clientScopeId, role)
+
+	if mappedRole == nil {
+		data.SetId("")
+	}
+
+	return nil
+}
+
+func resourceKeycloakGenericRoleMapperDelete(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	keycloakClient := meta.(*keycloak.KeycloakClient)
+
+	realmId := data.Get("realm_id").(string)
+	clientId := data.Get("client_id").(string)
+	clientScopeId := data.Get("client_scope_id").(string)
+	roleId := data.Get("role_id").(string)
+
+	role, err := keycloakClient.GetRole(ctx, realmId, roleId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diag.FromErr(keycloakClient.DeleteRoleScopeMapping(ctx, realmId, clientId, clientScopeId, role))
+}
+
+func resourceKeycloakGenericRoleMapperImport(_ context.Context, d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	parts := strings.Split(d.Id(), "/")
+
+	if len(parts) != 6 {
+		return nil, fmt.Errorf("Invalid import. Supported import formats: {{realmId}}/client/{{clientId}}/scope-mappings/{{roleClientId}}/{{roleId}}, {{realmId}}/client-scope/{{clientScopeId}}/scope-mappings/{{roleClientId}}/{{roleId}}")
+	}
+
+	parentResourceType := parts[1]
+	parentResourceId := parts[2]
+
+	d.Set("realm_id", parts[0])
+
+	if parentResourceType == "client" {
+		d.Set("client_id", parentResourceId)
+	} else if parentResourceType == "client-scope" {
+		d.Set("client_scope_id", parentResourceId)
+	} else {
+		return nil, fmt.Errorf("the associated parent resource must be either a client or a client-scope")
+	}
+
+	d.Set("role_id", parts[5])
+	return []*schema.ResourceData{d}, nil
+}
