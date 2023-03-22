ae3ea01ccf319fde61be2a80d76d50904f85812d
Michael Parker <michael@parker.gg>
2023-03-22 11:23:42 -0500

    ci: try waiting for kc to start via health check options

--- scripts/wait-for-local-keycloak.sh
+++ scripts/wait-for-local-keycloak.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 KEYCLOAK_URL="http://localhost:8080/"
 
 printf "Waiting for local Keycloak to be ready"
 
-until $(curl --output /dev/null --silent --head --fail --max-time 2 ${KEYCLOAK_URL}); do
+until $(curl --silent --head --fail --max-time 2 ${KEYCLOAK_URL}); do
     printf '.'
     sleep 2
 done
