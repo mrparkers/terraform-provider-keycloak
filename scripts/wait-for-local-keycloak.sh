27786f0c96be16552ffe52e7d34dbe7ec9a097f6
Michael Parker <michael@parker.gg>
2023-03-22 14:05:24 -0500

    fix: revert changes to script

--- scripts/wait-for-local-keycloak.sh
+++ scripts/wait-for-local-keycloak.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-set -x
-
 KEYCLOAK_URL="http://localhost:8080/"
 
 printf "Waiting for local Keycloak to be ready"
 
-until $(curl --silent --head --fail --max-time 2 ${KEYCLOAK_URL}); do
+until $(curl --output /dev/null --silent --head --fail --max-time 2 ${KEYCLOAK_URL}); do
     printf '.'
     sleep 2
 done
