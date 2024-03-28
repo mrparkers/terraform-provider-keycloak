956027603aba3c819ef4fa5a34e9da78e8bf4323
Dan Williams <j.dan.williams@universe.com>
2024-03-28 09:17:49 -0400

    Initial implementation `keycloak_group_members`

---
+++ provider/data_source_keycloak_group_members.go
@@ -0,0 +1,54 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
+)
+
+func dataSourceKeycloakGroupMembers() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceKeycloakGroupMembersRead,
+		Schema: map[string]*schema.Schema{
+			"realm_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"users": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceKeycloakGroupMembersRead(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	keycloakClient := meta.(*keycloak.KeycloakClient)
+
+	realmId := data.Get("realm_id").(string)
+	groupName := data.Get("name").(string)
+
+	users, err := keycloakClient.GetGroupMembers(ctx, realmId, groupName)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	usernames := make([]string, len(users))
+	for num, user := range users {
+		usernames[num] = user.Username
+	}
+
+	data.SetId(groupName)
+	data.Set("realm_id", realmId)
+	data.Set("name", groupName)
+	data.Set("users", usernames)
+
+	return nil
+}
