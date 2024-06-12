dfc89978cb44f5d721967900e9e4b2a6f4379c54
lucdew <lucdew@users.noreply.github.com>
2024-06-12 15:51:23 +0200

    Fix package import in provider_test.go

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
-	"github.com/mrkparkers/terraform-provider-keycloak/keycloak"
+	"github.com/mrparkers/terraform-provider-keycloak/keycloak"
 )
 
 var (
